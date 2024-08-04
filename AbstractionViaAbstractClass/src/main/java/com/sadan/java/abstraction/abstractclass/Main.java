1c23c6864f108a8da3fb63725bebe2133514f4c8
sadan <sadan.2507@gmail.com>
2024-08-04 08:35:29 +0530

    java abstraction example

---
+++ AbstractionViaAbstractClass/src/main/java/com/sadan/java/abstraction/abstractclass/Main.java
@@ -0,0 +1,21 @@
+package com.sadan.java.abstraction.abstractclass;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Car electricCar = new ElectricCar("BMW");
+        Car petrolCar = new PetrolCar("Audi");
+
+        System.out.println("Driving the electric car: " + electricCar.getCarType() + "\n");
+        electricCar.speedUp();
+        electricCar.turnLeft();
+        electricCar.slowDown();
+
+        System.out.println("\n\nDriving the petrol car: " + petrolCar.getCarType() + "\n");
+        petrolCar.slowDown();
+        petrolCar.turnRight();
+        petrolCar.turnLeft();
+    }
+
+}
---
+++ AbstractionViaInterface/src/main/java/com/sadan/java/usinginterface/Car.java
@@ -0,0 +1,14 @@
+package com.sadan.java.usinginterface;
+
+public interface Car {
+
+    public void speedUp();
+
+    public void slowDown();
+
+    public void turnRight();
+
+    public void turnLeft();
+    
+    public String getCarType();
+}
